2855e499345a747a49d2214a2a8e62a50a0f7adc
Ubuntu <ubuntu@ip-172-31-95-231.ec2.internal>
2020-02-13 11:19:24 +0000

    install tomcat server 9

---
+++ install_tomcat.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo wget http://apachemirror.wuchna.com/tomcat/tomcat-9/v9.0.31/bin/apache-tomcat-9.0.31.tar.gz
+tar -xvzf apache-tomcat-9.0.31.tar.gz
+rm apache-tomcat-9.0.31.tar.gz
+mv apache-tomcat-9.0.31/ tomcat9
